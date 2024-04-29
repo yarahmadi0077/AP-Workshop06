d274277867a0df114cf3a7dfe0fb0a86b44315fd
aynazmeymanat <aynazmeymanat@gmail.com>
2024-04-29 14:32:22 +0330

    2st commit of class Bird.java by aynazmeymanat

--- src/Eagle.java
+++ src/Eagle.java
@@ -1,2 +1,9 @@
  class Eagle extends Bird {
-}
+     public Eagle(String name, int age, String attributes, double heightOfFlight) {
+         super(name, age, attributes, heightOfFlight);
+     }
+
+     public String toString (){
+         return super.toString() ;
+     }
+ }
--- src/Parrot.java
+++ src/Parrot.java
@@ -1,2 +1,9 @@
 class Parrot extends Bird{
+    public Parrot(String name, int age, String attributes, double heightOfFlight) {
+        super(name, age, attributes, heightOfFlight);
+    }
+
+    public String toString(){
+        return super.toString() ;
+    }
 }
