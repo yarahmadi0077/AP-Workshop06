db524cca7599dae0992a383a47ab8fb0e8ff4610
aynazmeymanat <aynazmeymanat@gmail.com>
2024-04-29 13:53:54 +0330

    2st commit of class Animal.java by aynazmeymanat

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,42 @@
 class Animal {
+    private String name;
+    private int age;
+    private String attributes;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0)
+            this.age = age;
+    }
+
+    public String getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(String attributes) {
+        this.attributes = attributes;
+    }
+
+    public Animal(String name, int age, String attributes) {
+        this.name = name;
+        this.age = age;
+        this.attributes = attributes;
+    }
+    public String toString(){
+        return name + " " + age + " " + attributes ;
+    }
+    public void show (){
+        System.out.println( name + " "  + age + " " + attributes);
+    }
 }
